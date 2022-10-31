1cb80bd24cb605f48b648e6030d0e38655ed34c6
georgmaisser <georg.maisser@wunderbyte.at>
2022-10-31 23:25:19 +0100

    Step to import - matching of entities table is working, rest is to come

--- settings.php
+++ settings.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
 /**
- * Plugin administration pages are defined here.
+ * Plugin administration entities are defined here.
  *
  * @package     local_entities
  * @category    admin
@@ -35,12 +35,9 @@ if ($hassiteconfig) {
     $ADMIN->add($componentname, $settings);
 
     // Select Standard Categories from custom categories.
-
-    $array = ['2' => 'test3', '4' => 'test3'];
     $categories = \local_entities\customfield\entities_cf_helper::get_all_cf_categories();
 
     if (!empty($categories)) {
-        //array_unshift($categories, get_string('none', 'local_entities'));
         $settings->add(
             new admin_setting_configmultiselect(
             $componentname .'/categories',
